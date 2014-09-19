c64b42514725439338edcdf558b1e6d7866edd67
wgv-mpb13 <jesse@revivemarketing.org>
2014-09-19 10:48:10 -0500

    Created new file for loadingMask

---
+++ loadingMask.js
@@ -0,0 +1,92 @@
+/* MASK FUNCTION  */
+(function($) {
+		// Take an element and show thats its loading something
+		// and places a div over the top of the content to prevent interaction.
+		$.fn.loadingMask = function(options) {
+			var defaults = {
+				image : 'images/loading-dots.gif',
+				func : null,
+				text : 'Processing...'
+			};
+			options = $.extend(defaults, options);
+			options.opacity =  0.85;
+			options.bgcolor = '#F3F3F3';
+
+			return this.each( function() {
+				
+				// The element that will be loading
+				var $parent = $(this),
+				
+				// The div that make the item above appear to be loading
+				$mask = $('#loading_' + $parent.data('loading-id') );
+				
+                // locate the active Cloak
+		        this.mask = jQuery(document).find('.mask').get();
+        
+                // if it exists.. hide it!
+                if(this.mask) {
+                    jQuery('body').find('.mask').hide();
+                }
+
+                
+				// The loading div exists & is visible
+				// Hide it
+				if ( $mask.length && $mask.is(':visible') ) {
+
+					// remove class from parent
+					$parent.removeClass('mask-parent');
+					
+					// Hide the mask
+					$mask.hide();
+
+				// The loading div's not been created yet	
+				// Create the markup, insert into dom, position, then show	
+				} else if ( !( $mask.length ) ) {
+					
+					// A random ID
+					// We use this to asscocoiate the parent with its loading div
+					var id =  Math.floor( Math.random() * 10000 ),
+					
+					// The HTML to insert
+					mask_template = '<div class="mask" id="loading_' + id + '"><p>'  + 
+                        ( options.image ? '<img src="' + options.image+'" />' : '') + 
+                        ( options.text ? '<span>' + options.text + '</span>' : '') + 
+                        '</p></div>';
+					
+					// Add a data attribute to the parent & fade the content that is loading out
+					// this matches this parent to the id of the loading div
+					$parent.addClass('mask-parent').attr('data-loading-id', id );
+					
+					// Add the loading div, give it an id & position it over the parent.
+					$parent.prepend(mask_template);
+					
+                    $('#loading_' + id).css({
+                        'opacity' : options.opacity,
+                        'background-color' : options.bgcolor
+                    }).show();						
+								
+				
+				// The loading div exists but is hidden.
+				// Position it then show	
+				} else {
+					
+					// Fade the content that is loading out
+					$parent.addClass('mask-parent');
+
+					// Add the loading div, give it an id & position it over the parent.
+					$mask.show();					
+				}
+				// fire callback function, if it exists
+                if(options.func) { options.func();}
+			});			
+		},
+    $.fn.hideMask = function () {
+        // locate the active Cloak
+        this.mask = jQuery('body').find('.mask').get();
+           
+        // if it exists.. hide it!
+        if(this.mask) {
+            jQuery('body').find('.mask').hide();
+        }
+  	}
+})(jQuery);
--- plugin-logic.js
+++ plugin-logic.js
@@ -7,24 +7,49 @@ Plugin Logic:
 // Would like to be able to call the plugin using a link, with defined data('mask-target') 
 // OR
 // directly applying the mask to the element via: $(element_id).loadingMask('show'); and  $(element_id).loadingMask('hide');
+// Usage examples:
+//
+// LINK: clicking any link with class="load-mask" will init the click function below, loading the mask for a 5 second timeout.. then hiding it and redirecting to the orig. HREF
+//
+// <a href="http://google.com" data-mask-target="#element" class="load-mask">Show the Mask</a>
+//
+// $('.load-mask').on('click',function () {
+//	var href = $(this).attr('href'),
+//		target = $(this).data('mask-target');
+//	
+//		$(target).loadingMask('show');
+//    		setTimeout(function(){
+//      		$(target).loadingMask('hide');
+//          	window.location.href = href;
+//      	}, 5000);
+//		});  
+//		return false;
+// });
+
+// OR programmatically, calling the loadingMask() on a specified element
+
+// $("#target-id").loadingMask('show');
+// $("#target-id").loadingMask('hide');
 
 
 
 Name: loadingMask
 Options:
-	useImage: true // false // if true, use the image src provided next, else show the default spinner below
+	useImage: true // false // if true, use the image src provided next, else show the default spinner below assigned to $spinner
 	loadingImage: "../images/loader-dots.gif", // the image src used IF useImage is set to TRUE
-	loadingClass: "loading-bar-spinner"
+	loadingBar: '#loading-bar .bar', // var for loading bar element
+	spinnerClass: "loading-bar-spinner", // class for default spinner - used when useImage is FALSE
+	
 	
 	
 createMask: // create the DOM structure needed to display the mask
 
 	var id = Math.floor(Math.random() * 10000), // random ID, we use this to asscocoiate the parent with its loading div
-		$wrapper = '<div class="mask-parent" id="loading_' + id + '"></div>';
+		$wrapper = '<div class="mask" data-loading-id="' + id + '" id="loading_' + id + '"></div>';
 		$bar = '<div id="loading-bar"><div class="bar"><div class="peg"></div></div></div>';
-		$spinner = this.options.useImage ? $('<img src="' + this.options.loadingImage + '" />') : $('<div id="' + this.options.loadingClass + '"><div class="spinner-icon"></div></div>');
+		$spinner = this.options.useImage ? $('<img class="loading-spinner" src="' + this.options.loadingImage + '" />') : $('<div id="' + this.options.spinnerClass + '"><div class="spinner-icon"></div></div>');
 	
-	this.target.attr('data-loading-id', id); // add ID to the target element - used in CSS to apply position:relative for proper placement of mask below
+	this.target.addClass('mask-parent'); // add Class to the target element - used in CSS to apply position:relative for proper placement of mask below
 	$wrapper.prepend($bar); // make the bar the first child of the wrapper
 	$wrapper.append($spinner); // make the spinner the last child. Spinner HTML changes based on useImage option value!
 	return $wrapper;                    
@@ -36,20 +61,24 @@ loadingBar:
 
 	loadingBarWidth: function(){
 		var loadingBarPct = (n) + '%';
-        $('#loading-bar .bar').css('width', loadingBarPct);
+		
+        $(loadingBar).css('width', loadingBarPct);
         loadingBarStatus = n;
+
         clearTimeout(loadingBarTimeout);
         loadingBarTimeout = setTimeout(function() {
             _incLoadingBar();
         }, 250);
 	},
 	loadingBarIncrement: function(){
+		// if the Bar Status is at 100, then it's done.. stop returning
 		if (_loadingBarStatus() >= 100) {
             loadingCompleted();
             return;
         }
         var rnd = 0,
             stat = _loadingBarStatus();
+
         if (stat >= 0 && stat < 30) {
             // Start out between 10% increments
             rnd = 10;
@@ -69,9 +98,12 @@ loadingBar:
             // after 100%, don't increment:
             rnd = 0;
         }
+		
         var loadingBarPct = _loadingBarStatus() + rnd;
+		// change the bars width based on the results above 
         loadingBarWidth(loadingBarPct);
-
+		
+		// this is just for debug/testing purposes - load loadingBarPct into div#f
         $('#f').html('Percent: ' + loadingBarPct + '%');
 	},
 	loadingBarStatus: function(){
